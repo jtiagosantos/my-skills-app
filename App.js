88fdd830b2949b4b2c27b0f5b59e34e798ebf406
jtiagosantos <tiagosan040@gmail.com>
2022-07-25 20:49:18 -0300

    feat: 🎸 handle statusbar

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 //pages
 import { Home } from './src/pages/Home/Home';
 
-export const App = () => <Home />;
+export const App = () => {
+  return (
+    <>
+      <StatusBar backgroundColor='#121015' />
+      <Home />
+    </>
+  );
+};
